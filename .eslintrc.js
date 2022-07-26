c3c204fa8562bc0e3d140e905447e10f51e62068
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-07-26 22:45:15 +0530

    dawdwa

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
+  parser: "@babel/eslint-parser",
   settings: {
     next: {
       rootDir: ["apps/*/"],
