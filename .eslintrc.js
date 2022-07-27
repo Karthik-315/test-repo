639c4dbe15434225ea2cca4bb216066ad66a06a9
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-07-27 13:42:18 +0530

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
-  parser: "@babel/eslint-parser",
+  parser: "babel-eslint",
   settings: {
     next: {
       rootDir: ["apps/*/"],
