d1a2eabd75a12a53bb71a1df1b903d49b2512d58
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-07-27 14:01:40 +0530

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
   parser: "@babel/eslint-parser",
-  requireConfigFile: false,
+  parserOptions: {
+    requireConfigFile: false,
+  },
   babelOptions: {
     presets: ["@babel/preset-react"],
   },
