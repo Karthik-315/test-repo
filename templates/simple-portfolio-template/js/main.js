b23d186bf29500a8f629f8170ecb470b64c346e1
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-07-27 15:54:14 +0530

    no message

--- templates/simple-portfolio-template/js/main.js
+++ templates/simple-portfolio-template/js/main.js
@@ -14,7 +14,7 @@ const sliderButtonContainer = document.querySelector(
 
 const sliderIntervalTime = 5000;
 let somevar;
-let somevar2;
+  let somevar2;
 
 const copyrightYearContainer = document.querySelector(`.copyright-year`);
 
